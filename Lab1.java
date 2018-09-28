f10a2e53a45639c8fbd47f41574587d7937f7ca2
dsvti <dsvtigr007@gmail.com>
2018-09-28 11:22:39 +0300

    Commect 1.

--- Lab1.java
+++ Lab1.java
@@ -5,6 +5,7 @@ import java.sql.*;
 
 public class Lab1 {
 
+    //test
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/java";
